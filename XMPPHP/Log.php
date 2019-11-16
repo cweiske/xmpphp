cweiske/xmpphp

commit 57f33b4328aa764f8dc35bda6cb6a5736a40d9ba

Introduce PHP7 strict typing

XMPPHP/Log.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * XMPPHP: The PHP XMPP Library
  * Copyright (C) 2008  Nathanael C. Fritz
@@ -53,20 +52,20 @@ class Log
     /**
      * @var array
      */
-    protected $data = array();
+    protected $data = [];
 
     /**
      * @var array
      */
-    protected $names = array('ERROR', 'WARNING', 'INFO', 'DEBUG', 'VERBOSE');
+    protected $names = ['ERROR', 'WARNING', 'INFO', 'DEBUG', 'VERBOSE'];
 
     /**
-     * @var integer
+     * @var int
      */
     protected $runlevel;
 
     /**
-     * @var boolean
+     * @var bool
      */
     protected $printout;
 
@@ -74,11 +73,11 @@ class Log
      * Constructor
      *
      * @param boolean $printout
-     * @param int $runlevel
+     * @param int $runlevel (optional)
      */
-    public function __construct($printout = false, $runlevel = self::LEVEL_INFO)
+    public function __construct($printout = false, int $runlevel = self::LEVEL_INFO)
     {
-        $this->printout = (boolean)$printout;
+        $this->printout = (bool)$printout;
         $this->runlevel = (int)$runlevel;
     }
 
@@ -87,18 +86,23 @@ class Log
      * If printout in this instance is set to true, directly output the message
      *
      * @param string $msg
-     * @param integer $runlevel
+     * @param int $runlevel (optional)
      */
-    public function log($msg, $runlevel = self::LEVEL_INFO)
+    public function log($msg, $runlevel = self::LEVEL_INFO): void
     {
         $time = time();
-        #$this->data[] = array($this->runlevel, $msg, $time);
+        //$this->data[] = [$this->runlevel, $msg, $time];
         if ($this->printout and $runlevel <= $this->runlevel) {
             $this->writeLine($msg, $runlevel, $time);
         }
     }
 
-    protected function writeLine($msg, $runlevel, $time)
+    /**
+     * @param string $msg
+     * @param int $runlevel
+     * @param int $time
+     */
+    protected function writeLine(string $msg, int $runlevel, int $time): void
     {
         //echo date('Y-m-d H:i:s', $time)." [".$this->names[$runlevel]."]: ".$msg."\n";
         echo $time . " [" . $this->names[$runlevel] . "]: " . $msg . "\n";
@@ -109,10 +113,10 @@ class Log
      * Output the complete log.
      * Log will be cleared if $clear = true
      *
-     * @param boolean $clear
-     * @param integer $runlevel
+     * @param bool $clear
+     * @param int $runlevel
      */
-    public function printout($clear = true, $runlevel = null)
+    public function printout(bool $clear = true, int $runlevel = null): void
     {
         if ($runlevel === null) {
             $runlevel = $this->runlevel;
@@ -123,7 +127,7 @@ class Log
             }
         }
         if ($clear) {
-            $this->data = array();
+            $this->data = [];
         }
     }
 }

XMPPHP/XMPP.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * XMPPHP: The PHP XMPP Library
  * Copyright (C) 2008  Nathanael C. Fritz
@@ -59,7 +58,7 @@ class XMPP extends XMLStream
      */
     public $user;
     /**
-     * @var boolean
+     * @var bool
      */
     public $track_presence = true;
     /**
@@ -83,16 +82,16 @@ class XMPP extends XMLStream
      */
     protected $basejid;
     /**
-     * @var boolean
+     * @var bool
      */
     protected $authed = false;
     protected $session_started = false;
     /**
-     * @var boolean
+     * @var bool
      */
     protected $auto_subscribe = false;
     /**
-     * @var boolean
+     * @var bool
      */
     protected $use_encryption = true;
 
@@ -105,12 +104,20 @@ class XMPP extends XMLStream
      * @param string $password
      * @param string $resource
      * @param string $server
-     * @param boolean $printlog
-     * @param string $loglevel
+     * @param bool $print_log
+     * @param string $log_level
      */
-    public function __construct($host, $port, $user, $password, $resource, $server = null, $printlog = false, $loglevel = null)
-    {
-        parent::__construct($host, $port, $printlog, $loglevel);
+    public function __construct(
+        string $host,
+        int $port,
+        string $user,
+        string $password,
+        string $resource,
+        ?string $server = null,
+        bool $print_log = false,
+        ?string $log_level = null
+    ) {
+        parent::__construct($host, $port, $print_log, $log_level);
 
         $this->user = $user;
         $this->password = $password;
@@ -140,9 +147,9 @@ class XMPP extends XMLStream
     /**
      * Turn encryption on/ff
      *
-     * @param boolean $useEncryption
+     * @param bool $useEncryption (optional)
      */
-    public function useEncryption($useEncryption = true)
+    public function useEncryption(bool $useEncryption = true): void
     {
         $this->use_encryption = $useEncryption;
     }
@@ -150,9 +157,9 @@ class XMPP extends XMLStream
     /**
      * Turn on auto-authorization of subscription requests.
      *
-     * @param boolean $autoSubscribe
+     * @param bool $autoSubscribe (optional)
      */
-    public function autoSubscribe($autoSubscribe = true)
+    public function autoSubscribe(bool $autoSubscribe = true): void
     {
         $this->auto_subscribe = $autoSubscribe;
     }
@@ -162,12 +169,12 @@ class XMPP extends XMLStream
      *
      * @param string $to
      * @param string $body
-     * @param string $type
-     * @param string $subject
-     * @param null $payload
+     * @param string $type (optional)
+     * @param string|null $subject (optional)
+     * @param string|null $payload (optional)
      * @throws Exception
      */
-    public function message($to, $body, $type = 'chat', $subject = null, $payload = null)
+    public function message(string $to, string $body, string $type = 'chat', ?string $subject = null, ?string $payload = null): void
     {
         if ($this->disconnected) {
             throw new Exception('You need to connect first');
@@ -177,13 +184,13 @@ class XMPP extends XMLStream
             $type = 'chat';
         }
 
-        $to      = htmlspecialchars($to);
-        $body    = htmlspecialchars($body);
+        $to = htmlspecialchars($to);
+        $body = htmlspecialchars($body);
         $subject = htmlspecialchars($subject);
         $subject = ($subject) ? '<subject>' . $subject . '</subject>' : '';
         $payload = ($payload) ? $payload : '';
         $sprintf = '<message from="%s" to="%s" type="%s">%s<body>%s</body>%s</message>';
-        $output  = sprintf($sprintf, $this->fulljid, $to, $type, $subject, $body, $payload);
+        $output = sprintf($sprintf, $this->fulljid, $to, $type, $subject, $body, $payload);
         $this->send($output);
     }
 
@@ -197,7 +204,7 @@ class XMPP extends XMLStream
      * @param null $priority
      * @throws Exception
      */
-    public function presence($status = null, $show = 'available', $to = null, $type = 'available', $priority = null)
+    public function presence($status = null, $show = 'available', $to = null, $type = 'available', $priority = null): void
     {
         if ($this->disconnected) {
             throw new Exception('You need to connect first');
@@ -242,8 +249,9 @@ class XMPP extends XMLStream
      * Send Auth request
      *
      * @param string $jid
+     * @throws Exception
      */
-    public function subscribe($jid)
+    public function subscribe(string $jid): void
     {
         $this->send("<presence type='subscribe' to='{$jid}' from='{$this->fulljid}' />");
         #$this->send("<presence type='subscribed' to='{$jid}' from='{$this->fulljid}' />");
@@ -254,7 +262,7 @@ class XMPP extends XMLStream
      *
      * @param string $xml
      */
-    public function message_handler($xml)
+    public function message_handler(string $xml): void
     {
         if (isset($xml->attrs['type'])) {
             $payload['type'] = $xml->attrs['type'];
@@ -273,8 +281,9 @@ class XMPP extends XMLStream
      * Presence handler
      *
      * @param string $xml
+     * @throws Exception
      */
-    public function presence_handler($xml)
+    public function presence_handler(string $xml): void
     {
         $payload['type'] = (isset($xml->attrs['type'])) ? $xml->attrs['type'] : 'available';
         $payload['show'] = (isset($xml->sub('show')->data)) ? $xml->sub('show')->data : $payload['type'];
@@ -302,8 +311,9 @@ class XMPP extends XMLStream
     /**
      * Retrieves the roster
      *
+     * @throws Exception
      */
-    public function getRoster()
+    public function getRoster(): void
     {
         $id = $this->getID();
         $this->send("<iq xmlns='jabber:client' type='get' id='$id'><query xmlns='jabber:iq:roster' /></iq>");
@@ -311,9 +321,10 @@ class XMPP extends XMLStream
 
     /**
      * Retrieves the vcard
-     * @param null $jid
+     * @param string|null $jid
+     * @throws Exception
      */
-    public function getVCard($jid = null)
+    public function getVCard(?string $jid = null): void
     {
         $id = $this->getID();
         $this->addIdHandler($id, 'vcard_get_handler');
@@ -328,8 +339,9 @@ class XMPP extends XMLStream
      * Features handler
      *
      * @param string $xml
+     * @throws Exception
      */
-    protected function features_handler($xml)
+    protected function features_handler(string $xml): void
     {
         if ($xml->hasSub('starttls') and $this->use_encryption) {
             $this->send("<starttls xmlns='urn:ietf:params:xml:ns:xmpp-tls'><required /></starttls>");
@@ -351,8 +363,9 @@ class XMPP extends XMLStream
      * SASL success handler
      *
      * @param string $xml
+     * @throws Exception
      */
-    protected function sasl_success_handler($xml)
+    protected function sasl_success_handler(string $xml): void
     {
         $this->log->log("Auth success!");
         $this->authed = true;
@@ -365,7 +378,7 @@ class XMPP extends XMLStream
      * @param string $xml
      * @throws Exception
      */
-    protected function sasl_failure_handler($xml)
+    protected function sasl_failure_handler(string $xml): void
     {
         $this->log->log("Auth failed!", Log::LEVEL_ERROR);
         $this->disconnect();
@@ -377,8 +390,9 @@ class XMPP extends XMLStream
      * Resource bind handler
      *
      * @param string $xml
+     * @throws Exception
      */
-    protected function resource_bind_handler($xml)
+    protected function resource_bind_handler(string $xml): void
     {
         if ($xml->attrs['type'] == 'result') {
             $this->log->log("Bound to " . $xml->sub('bind')->sub('jid')->data);
@@ -396,13 +410,15 @@ class XMPP extends XMLStream
      * Gets all packets matching XPath "iq/{jabber:iq:roster}query'
      *
      * @param string $xml
+     * @throws Exception
      */
-    protected function roster_iq_handler($xml)
+    protected function roster_iq_handler(string $xml): void
     {
         $status = "result";
         $xmlroster = $xml->sub('query');
+        $contacts = [];
         foreach ($xmlroster->subs as $item) {
-            $groups = array();
+            $groups = [];
             if ($item->name == 'item') {
                 $jid = $item->attrs['jid']; //REQUIRED
                 $name = $item->attrs['name']; //MAY
@@ -412,7 +428,7 @@ class XMPP extends XMLStream
                         $groups[] = $subitem->data;
                     }
                 }
-                $contacts[] = array($jid, $subscription, $name, $groups); //Store for action if no errors happen
+                $contacts[] = [$jid, $subscription, $name, $groups]; //Store for action if no errors happen
             } else {
                 $status = "error";
             }
@@ -432,7 +448,7 @@ class XMPP extends XMLStream
      *
      * @param string $xml
      */
-    protected function session_start_handler($xml)
+    protected function session_start_handler(string $xml): void
     {
         $this->log->log("Session started");
         $this->session_started = true;
@@ -443,8 +459,9 @@ class XMPP extends XMLStream
      * TLS proceed handler
      *
      * @param string $xml
+     * @throws Exception
      */
-    protected function tls_proceed_handler($xml)
+    protected function tls_proceed_handler(string $xml): void
     {
         $this->log->log("Starting TLS encryption");
         stream_socket_enable_crypto($this->socket, true, STREAM_CRYPTO_METHOD_TLS_CLIENT);
@@ -456,14 +473,14 @@ class XMPP extends XMLStream
      *
      * @param XMLObj $xml
      */
-    protected function vcard_get_handler($xml)
+    protected function vcard_get_handler(XMLObj $xml): void
     {
-        $vcard_array = array();
+        $vcard_array = [];
         $vcard = $xml->sub('vcard');
         // go through all of the sub elements and add them to the vcard array
         foreach ($vcard->subs as $sub) {
             if ($sub->subs) {
-                $vcard_array[$sub->name] = array();
+                $vcard_array[$sub->name] = [];
                 foreach ($sub->subs as $sub_child) {
                     $vcard_array[$sub->name][$sub_child->name] = $sub_child->data;
                 }

XMPPHP/BOSH.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * XMPPHP: The PHP XMPP Library
  * Copyright (C) 2008  Nathanael C. Fritz
@@ -35,7 +34,7 @@ namespace XMPPHP;
 use SimpleXMLElement;
 
 /** XMPPHP_XMLStream */
-require_once __DIR__ . "/XMPP.php";
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'XMPP.php';
 
 /**
  * XMPPHP BOSH
@@ -51,7 +50,7 @@ require_once __DIR__ . "/XMPP.php";
 class BOSH extends XMPP
 {
     /**
-     * @var integer
+     * @var int
      */
     protected $rid;
 
@@ -68,7 +67,7 @@ class BOSH extends XMPP
     /**
      * @var array
      */
-    protected $http_buffer = array();
+    protected $http_buffer = [];
 
     /**
      * @var string
@@ -76,50 +75,58 @@ class BOSH extends XMPP
     protected $session = false;
 
     /**
-     * @var integer
+     * @var int
      */
     protected $inactivity;
 
-    /**
-     * Connect
-     *
-     * @param $server
-     * @param $wait
-     * @param $session
-     * @throws Exception
-     * @throws Exception
-     */
-    public function connect($server = null, $wait = '1', $session = false)
-    {
-
+    public function __construct(
+        string $host,
+        int $port,
+        string $user,
+        string $password,
+        string $resource,
+        ?string $server = null,
+        bool $print_log = false,
+        ?string $log_level = null
+    ) {
+        parent::__construct($host, $port, $user, $password, $resource, $server, $print_log, $log_level);
         if (is_null($server)) {
-
             // If we aren't given the server http url, try and guess it
-            $port_string = ($this->port AND $this->port != 80) ? ':' . $this->port : '';
+            $port_string = ($this->port and $this->port != 80) ? ':' . $this->port : '';
             $this->http_server = 'http://' . $this->host . $port_string . '/http-bind/';
         } else {
             $this->http_server = $server;
         }
+    }
 
+    /**
+     * Connect
+     *
+     * @param bool $persistent
+     * @param bool $send_init
+     * @param int $timeout
+     * @throws Exception
+     */
+    public function connect(bool $persistent = false, bool $send_init = true, int $timeout = 30): void
+    {
         $this->use_encryption = false;
-        $this->session = $session;
+        $this->session = $persistent;
         $this->rid = 3001;
         $this->sid = null;
         $this->inactivity = 0;
 
-        if ($session) {
+        if ($persistent) {
             $this->loadSession();
         }
 
         if (!$this->sid) {
-
             $body = $this->__buildBody();
             $body->addAttribute('hold', '1');
             $body->addAttribute('to', $this->server);
             $body->addAttribute('route', 'xmpp:' . $this->host . ':' . $this->port);
             $body->addAttribute('secure', 'true');
             $body->addAttribute('xmpp:version', '1.0', 'urn:xmpp:xbosh');
-            $body->addAttribute('wait', strval($wait));
+            $body->addAttribute('wait', strval($timeout));
             $body->addAttribute('ack', '1');
             $body->addAttribute('xmlns:xmpp', 'urn:xmpp:xbosh');
             $buff = '<stream:stream xmlns="jabber:client" xmlns:stream="http://etherx.jabber.org/streams">';
@@ -138,11 +145,9 @@ class BOSH extends XMPP
      * Load session
      *
      */
-    public function loadSession()
+    public function loadSession(): void
     {
-
         if ($this->session == 'ON_FILE') {
-
             // Session not started so use session_file
             $session_file = $this->getSessionFile();
 
@@ -169,7 +174,6 @@ class BOSH extends XMPP
         $this->lat = (time() - (isset($_SESSION['XMPPHP_BOSH']['lat']))) ? $_SESSION['XMPPHP_BOSH']['lat'] : 0;
 
         if ($this->lat < $this->inactivity) {
-
             if (isset($_SESSION['XMPPHP_BOSH']['RID'])) {
                 $this->rid = $_SESSION['XMPPHP_BOSH']['RID'];
             }
@@ -189,10 +193,10 @@ class BOSH extends XMPP
     }
 
     /**
-     * Get the session file
+     * Get the session file location
      *
      */
-    public function getSessionFile()
+    public function getSessionFile(): string
     {
         return sys_get_temp_dir() . '/' . $this->user . '_' . $this->server . '_session';
     }
@@ -200,17 +204,15 @@ class BOSH extends XMPP
     /**
      * Build body
      *
-     * @param $sub
-     * @return SimpleXMLElement|string
+     * @param SimpleXMLElement|null $sub
+     * @return SimpleXMLElement
      */
-    public function __buildBody($sub = null)
+    private function __buildBody(?SimpleXMLElement $sub = null): SimpleXMLElement
     {
-
-        $xml = '<body xmlns="http://jabber.org/protocol/httpbind" xmlns:xmpp="urn:xmpp:xbosh" />';
-        $xml = new SimpleXMLElement($xml);
+        $xml = new SimpleXMLElement('<body xmlns="http://jabber.org/protocol/httpbind" xmlns:xmpp="urn:xmpp:xbosh" />');
         $xml->addAttribute('content', 'text/xml; charset=utf-8');
         $xml->addAttribute('rid', $this->rid);
-        $this->rid++;
+        ++$this->rid;
         if ($this->sid) {
             $xml->addAttribute('sid', $this->sid);
         }
@@ -218,7 +220,6 @@ class BOSH extends XMPP
         $xml->addAttribute('xml:lang', 'en');
 
         if ($sub !== null) {
-
             // Ok, so simplexml is lame
             $parent = dom_import_simplexml($xml);
             $content = dom_import_simplexml($sub);
@@ -233,21 +234,19 @@ class BOSH extends XMPP
     /**
      * Send body
      *
-     * @param $body
-     * @param $recv
+     * @param SimpleXMLElement|null $body
+     * @param bool $recv
      * @return bool|string
      * @throws Exception
-     * @throws Exception
      */
-    public function __sendBody($body = null, $recv = true)
+    private function __sendBody(?SimpleXMLElement $body = null, bool $recv = true)
     {
-
         if (!$body) {
             $body = $this->__buildBody();
         }
 
         $output = '';
-        $header = array('Accept-Encoding: gzip, deflate', 'Content-Type: text/xml; charset=utf-8');
+        $header = ['Accept-Encoding: gzip, deflate', 'Content-Type: text/xml; charset=utf-8'];
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, $this->http_server);
@@ -283,9 +282,8 @@ class BOSH extends XMPP
      * @throws Exception
      * @throws Exception
      */
-    public function __process($null1 = null, $null2 = null)
+    private function __process($null1 = null, $null2 = null)
     {
-
         if ($this->http_buffer) {
             $this->__parseBuffer();
         } else {
@@ -298,17 +296,14 @@ class BOSH extends XMPP
         return true;
     }
 
-    public function __parseBuffer()
+    private function __parseBuffer()
     {
-
         while ($this->http_buffer) {
-
             $idx = key($this->http_buffer);
             $buffer = $this->http_buffer[$idx];
             unset($this->http_buffer[$idx]);
 
             if ($buffer) {
-
                 $xml = new SimpleXMLElement($buffer);
                 $children = $xml->xpath('child::node()');
 
@@ -325,9 +320,8 @@ class BOSH extends XMPP
      * Save session
      *
      */
-    public function saveSession()
+    public function saveSession(): void
     {
-
         $_SESSION['XMPPHP_BOSH']['RID'] = (string)$this->rid;
         $_SESSION['XMPPHP_BOSH']['SID'] = (string)$this->sid;
         $_SESSION['XMPPHP_BOSH']['authed'] = (boolean)$this->authed;
@@ -337,7 +331,6 @@ class BOSH extends XMPP
         $_SESSION['XMPPHP_BOSH']['lat'] = (string)time();
 
         if ($this->session == 'ON_FILE') {
-
             $session_file = $this->getSessionFile();
             $session_file_fp = fopen($session_file, 'r');
             flock($session_file_fp, LOCK_EX);
@@ -353,13 +346,10 @@ class BOSH extends XMPP
      * Process
      *
      * @param $msg
-     * @param $null
-     *
-     * null param are not used and just to statify Strict Function Declaration
-     * @throws Exception
+     * @param int|null $_ unused
      * @throws Exception
      */
-    public function send($msg, $null = null)
+    public function send($msg, ?int $_ = null)
     {
         $this->log->log('SEND: ' . $msg, Log::LEVEL_VERBOSE);
         $msg = new SimpleXMLElement($msg);
@@ -371,18 +361,17 @@ class BOSH extends XMPP
      *
      * @throws Exception
      */
-    public function reset()
+    public function reset(): void
     {
-
         $this->xml_depth = 0;
         unset($this->xmlobj);
-        $this->xmlobj = array();
+        $this->xmlobj = [];
         $this->setupParser();
         $body = $this->__buildBody();
         $body->addAttribute('to', $this->host);
         $body->addAttribute('xmpp:restart', 'true', 'urn:xmpp:xbosh');
         $buff = '<stream:stream xmlns="jabber:client" xmlns:stream="http://etherx.jabber.org/streams">';
-        $response = $this->__sendBody($body);
+        $this->__sendBody($body);
         $this->been_reset = true;
         xml_parse($this->parser, $buff, false);
     }
@@ -392,15 +381,14 @@ class BOSH extends XMPP
      *
      * @throws Exception
      */
-    public function disconnect()
+    public function disconnect(): void
     {
-
         parent::disconnect();
 
         if ($this->session == 'ON_FILE') {
             unlink($this->getSessionFile());
         } else {
-            $keys = array('RID', 'SID', 'authed', 'basejid', 'fulljid', 'inactivity', 'lat');
+            $keys = ['RID', 'SID', 'authed', 'basejid', 'fulljid', 'inactivity', 'lat'];
             foreach ($keys as $key) {
                 unset($_SESSION['XMPPHP_BOSH'][$key]);
             }

XMPPHP/Exception.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * XMPPHP: The PHP XMPP Library
  * Copyright (C) 2008  Nathanael C. Fritz

commit d95381f8fb5ffbbda0894aea415742daaa2eab22

Fix types related issues introduced with 57f33b4

XMPPHP/Log.php
@@ -75,10 +75,10 @@ class Log
      * @param boolean $printout
      * @param int $runlevel (optional)
      */
-    public function __construct($printout = false, int $runlevel = self::LEVEL_INFO)
+    public function __construct($printout = false, ?int $runlevel = self::LEVEL_INFO)
     {
-        $this->printout = (bool)$printout;
-        $this->runlevel = (int)$runlevel;
+        $this->printout = (bool) $printout;
+        $this->runlevel = (int) ($runlevel ?? 0);
     }
 
     /**

XMPPHP/XMLStream.php
@@ -581,9 +581,9 @@ class XMLStream
      *
      * @param string $name
      * @param string $pointer
-     * @param string $obj
+     * @param object $obj
      */
-    public function addEventHandler(string $name, string $pointer, string $obj)
+    public function addEventHandler(string $name, string $pointer, object $obj)
     {
         $this->eventhandlers[] = [$name, $pointer, $obj];
     }
@@ -644,12 +644,12 @@ class XMLStream
     /**
      * XML start callback
      *
-     * @param string resource $parser
+     * @param resource $parser
      * @param string $name
      * @param array $attr
      * @see xml_set_element_handler
      */
-    public function startXML(string $parser, string $name, array $attr): void
+    public function startXML($parser, string $name, array $attr): void
     {
         if ($this->been_reset) {
             $this->been_reset = false;
@@ -687,13 +687,13 @@ class XMLStream
     /**
      * XML end callback
      *
-     * @param string resource $parser
+     * @param resource $parser
      * @param string $name
      * @throws Exception
      * @see xml_set_element_handler
      *
      */
-    public function endXML(string $parser, string $name): void
+    public function endXML($parser, string $name): void
     {
         #$this->log->log("Ending $name",  Log::LEVEL_DEBUG);
         #print "$name\n";
@@ -780,12 +780,12 @@ class XMLStream
 
     /**
      * XML character callback
-     * @param string resource $parser
+     * @param resource $parser
      * @param string $data
      * @see xml_set_character_data_handler
      *
      */
-    public function charXML(string $parser, string $data): void
+    public function charXML($parser, string $data): void
     {
         if (array_key_exists($this->xml_depth, $this->xmlobj)) {
             $this->xmlobj[$this->xml_depth]->data .= $data;

XMPPHP/XMPP.php
@@ -260,9 +260,9 @@ class XMPP extends XMLStream
     /**
      * Message handler
      *
-     * @param string $xml
+     * @param XMLObj $xml
      */
-    public function message_handler(string $xml): void
+    public function message_handler(XMLObj $xml): void
     {
         if (isset($xml->attrs['type'])) {
             $payload['type'] = $xml->attrs['type'];
@@ -280,10 +280,10 @@ class XMPP extends XMLStream
     /**
      * Presence handler
      *
-     * @param string $xml
+     * @param XMLObj $xml
      * @throws Exception
      */
-    public function presence_handler(string $xml): void
+    public function presence_handler(XMLObj $xml): void
     {
         $payload['type'] = (isset($xml->attrs['type'])) ? $xml->attrs['type'] : 'available';
         $payload['show'] = (isset($xml->sub('show')->data)) ? $xml->sub('show')->data : $payload['type'];
@@ -338,10 +338,10 @@ class XMPP extends XMLStream
     /**
      * Features handler
      *
-     * @param string $xml
+     * @param XMLObj $xml
      * @throws Exception
      */
-    protected function features_handler(string $xml): void
+    protected function features_handler(XMLObj $xml): void
     {
         if ($xml->hasSub('starttls') and $this->use_encryption) {
             $this->send("<starttls xmlns='urn:ietf:params:xml:ns:xmpp-tls'><required /></starttls>");
@@ -362,10 +362,10 @@ class XMPP extends XMLStream
     /**
      * SASL success handler
      *
-     * @param string $xml
+     * @param XMLObj $xml
      * @throws Exception
      */
-    protected function sasl_success_handler(string $xml): void
+    protected function sasl_success_handler(XMLObj $xml): void
     {
         $this->log->log("Auth success!");
         $this->authed = true;
@@ -375,10 +375,10 @@ class XMPP extends XMLStream
     /**
      * SASL feature handler
      *
-     * @param string $xml
+     * @param XMLObj $xml
      * @throws Exception
      */
-    protected function sasl_failure_handler(string $xml): void
+    protected function sasl_failure_handler(XMLObj $xml): void
     {
         $this->log->log("Auth failed!", Log::LEVEL_ERROR);
         $this->disconnect();
@@ -389,10 +389,10 @@ class XMPP extends XMLStream
     /**
      * Resource bind handler
      *
-     * @param string $xml
+     * @param XMLObj $xml
      * @throws Exception
      */
-    protected function resource_bind_handler(string $xml): void
+    protected function resource_bind_handler(XMLObj $xml): void
     {
         if ($xml->attrs['type'] == 'result') {
             $this->log->log("Bound to " . $xml->sub('bind')->sub('jid')->data);
@@ -409,10 +409,10 @@ class XMPP extends XMLStream
      * Roster iq handler
      * Gets all packets matching XPath "iq/{jabber:iq:roster}query'
      *
-     * @param string $xml
+     * @param XMLObj $xml
      * @throws Exception
      */
-    protected function roster_iq_handler(string $xml): void
+    protected function roster_iq_handler(XMLObj $xml): void
     {
         $status = "result";
         $xmlroster = $xml->sub('query');
@@ -446,9 +446,9 @@ class XMPP extends XMLStream
     /**
      * Session start handler
      *
-     * @param string $xml
+     * @param XMLObj $xml
      */
-    protected function session_start_handler(string $xml): void
+    protected function session_start_handler(XMLObj $xml): void
     {
         $this->log->log("Session started");
         $this->session_started = true;
@@ -458,10 +458,10 @@ class XMPP extends XMLStream
     /**
      * TLS proceed handler
      *
-     * @param string $xml
+     * @param XMLObj $xml
      * @throws Exception
      */
-    protected function tls_proceed_handler(string $xml): void
+    protected function tls_proceed_handler(XMLObj $xml): void
     {
         $this->log->log("Starting TLS encryption");
         stream_socket_enable_crypto($this->socket, true, STREAM_CRYPTO_METHOD_TLS_CLIENT);